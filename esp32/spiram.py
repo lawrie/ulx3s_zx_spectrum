662d0e481128310f796294c23ba8efccdf73396e
lawrie <lawrie.griffiths@ntlworld.com>
2020-04-23 19:11:12 +0100

    Use addresses consistently

--- esp32/spiram.py
+++ esp32/spiram.py
@@ -66,12 +66,14 @@ class spiram:
     self.hwspi.write(bytearray([0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x00]))
     self.led.off()
 
-def load(filename, addr=0):
+def load(filename, addr=0x4000):
   s=spiram()
+  addr -= 0x4000
   s.load_stream(open(filename, "rb"), addr)
 
-def save(filename, addr=0, length=1024):
+def save(filename, addr=0x4000, length=0xC000):
   s=spiram()
+  addr -= 0x4000
   f=open(filename, "wb")
   s.save_stream(f, addr, length)
   f.close()
@@ -104,7 +106,3 @@ def help():
   print("spiram.load(\"file.bin\",addr=0)")
   print("spiram.save(\"file.bin\",addr=0,length=1024)")
 
-# debug to manually write and read 4 bytes
-#d=spiram.spiram()
-#d.led.on(); d.hwspi.write(bytearray([0x00,0x00,0x00,0x40,0x41,0x42,0x43])); d.led.off()
-#d.led.on(); d.hwspi.write(bytearray([0x01,0x00,0x00,0x00])); print(d.hwspi.read(4)); d.led.off()
